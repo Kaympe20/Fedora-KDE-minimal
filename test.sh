80ee438b18456475c5e09e128284d5f18b4d93d2
Champe20 <117483128+Champe20@users.noreply.github.com>
2023-06-07 17:01:12 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -27,11 +27,3 @@ PKGS=(
 	"libreoffice-writer"
 	"pim"
 )
-sudo dnf -y remove "${PKGS[@]}"
-echo "Do you wish to reinstall Firefox as to remove all of Fedora's changes?"
-select yn in "Yes" "No"; do
-    case $yn in
-        Yes ) sudo dnf -y remove firefox; sudo dnf -y install firefox;;
-        No ) exit;;
-    esac
- done
